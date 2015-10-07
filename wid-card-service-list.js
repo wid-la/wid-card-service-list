651b3465041a4515308e0ad1d10fd42bdbb3f136
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-06 20:01:56 -0700

    Styling of element.

--- wid-card-service-list.js
+++ wid-card-service-list.js
@@ -6,7 +6,46 @@
 
     is: 'wid-card-service-list',
 
-    properties: {}
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+
+    properties: {
+      /**
+       * engines
+       * @type {Object}
+       */
+      engines: {
+        type: Object
+      },
+
+      /**
+       * status
+       * @type {Object}
+       */
+      status: {
+        type: Object
+      }
+
+    },
+
+    _rowClass: function _rowClass(index) {
+      var odd = '';
+      if (index % 2 === 0) {
+        odd = 'odd';
+      }
+      return odd + ' flex layout horizontal center-center';
+    },
+
+    _statusClass: function _statusClass(status) {
+      if (status === 1) {
+        return 'green';
+      } else {
+        return 'red';
+      }
+    },
+
+    _iconPath: function _iconPath(value) {
+      return '/images/cards/' + value;
+    }
 
   });
 })();
