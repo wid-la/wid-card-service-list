fe4d74ebe87ee96895c454a37e045587e26802ce
Aurelien Chaudagne <ouralien@gmail.com>
2015-11-28 12:02:44 -0800

    Adds Theme Behavior.

--- wid-card-service-list.js
+++ wid-card-service-list.js
@@ -6,7 +6,7 @@
 
     is: 'wid-card-service-list',
 
-    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior, Polymer.WidCardThemeBehavior],
 
     properties: {
       /**
